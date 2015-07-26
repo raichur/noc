f5c7780265eeec23d59c6845a2a3ec79d5f62c04
Joshua Raichur <hello@joshuaraichur.com>
2015-07-26 16:39:20 +0530

    Add: Vector normalization

---
+++ PVector/5. Vector normalization/build/build/source/build.java
@@ -0,0 +1,42 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class build extends PApplet {
+
+public void setup() {
+  size(640,360);
+}
+
+public void draw() {
+  background(255);
+  PVector mouse = new PVector(mouseX,mouseY);
+  PVector center = new PVector(width/2,height/2);
+
+  mouse.sub(center);
+  // To normalize a vector is to leave its direction as is
+  // but make its magnitude 1 unit
+  mouse.normalize();
+
+  translate(width/2,height/2);
+  line(0,0,mouse.x,mouse.y);
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
